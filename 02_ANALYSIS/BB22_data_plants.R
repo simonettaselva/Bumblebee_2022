a4627015bfb81173ef5fabe4da98db983fabfd97
simonettaselva <sselva@student.ethz.ch>
2022-11-09 11:26:31 +0100

    reconnect

--- 02_ANALYSIS/BB22_data_plants.R
+++ 02_ANALYSIS/BB22_data_plants.R
@@ -5,6 +5,7 @@
 # Created: October 19, 2022
 # Project: Bumblebee 2022
 ################################################
+rm(list=ls())
 
 #load libraries
 library(dplyr)
@@ -17,8 +18,30 @@ library(ggpubr)
 input <- "~/Library/CloudStorage/GoogleDrive-simo1996s@gmail.com/My Drive/ETH/Master Thesis/Bumblebee_2022/01_DATA"
 output <- "~/Library/CloudStorage/GoogleDrive-simo1996s@gmail.com/My Drive/ETH/Master Thesis/Bumblebee_2022/03_OUTPUT"
 
+
 # load data
 setwd(input)
+BB22 <- read.csv("BB22_data_tres_0.01.csv")
+BB22 <- BB22%>% 
+  mutate(binom.abund = if_else(Abundance == 0, 0, 1),
+         ID = substring(Sample, 2),
+         site = paste(location, replicate, sep="_"),
+         bbspecies = as_factor(bbspecies),
+         OTU = as_factor(OTU))
+levels(BB22$bbspecies) <- c("B.lapidarius", "B.pascuorum")
+
+BB22.plantspecies <- unique(BB22$OTU)
+
+#import Joans dataset
+JC_floral_traits <- read.csv2("floral_traits.csv",sep = ",")%>% 
+  mutate(Sp.merge = as_factor(Sp.merge))
+
+# see how many of hte plant species are shared by datasets
+shared.species <- length(intersect(JC.plantspecies,BB22.plantspecies))
+# 169 are shared
+
+
+###############
 plants <- read.csv2("phenology_pollen_nectar_sugar_database_copy.csv",sep = ",")
 plants.sum <- plants%>% 
   summarize(plant.species = plant.species,
@@ -38,3 +61,9 @@ plants.sum <- plants%>%
             sugar.concentration...in.nectar = sugar.concentration...in.nectar,
             pollen.flower = total.pollen.per.flower..the.number.of.grains.)
 names(plants)
+
+
+
+
+
+
