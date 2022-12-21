1be3b52b6e9a7960b05560585d593a4fa46723c3
simonettaselva <sselva@student.ethz.ch>
2022-12-21 17:47:27 +0100

    improving plots, add icons, color phylo tree in

--- 02_ANALYSIS/BB22_Goal_0_script.R
+++ 02_ANALYSIS/BB22_Goal_0_script.R
@@ -43,6 +43,200 @@ BB22.full$region <- c()
 BB22.full.leg <- BB22.full[BB22.full$bborgan=="L",]
 BB22.full.body <- BB22.full[BB22.full$bborgan=="B",]
 
+#### BOTH TYPE OF POLLEN ####
+#### plot plant families per site, region and species
+
+# 1. find the 30 most abundant families
+families.overview <- BB22.full%>%
+  dplyr::group_by(family)%>%
+  dplyr::summarise(cum.abund = sum(Abundance))
+tot <- sum(families.overview$cum.abund)
+families.overview <- families.overview %>%
+  dplyr::summarise(family = family,
+                   cum.abund = cum.abund,
+                   rel.abund = cum.abund/tot)
+# plot family abundance
+ggplot(families.overview, aes(y=reorder(family, -cum.abund), x=cum.abund)) + 
+  geom_bar(stat="identity")+ theme_classic()
+
+# use cumulative abundance to select 30 most abundant species
+rare.families <- families.overview %>% top_n(nrow(families.overview)-30, -cum.abund)
+BB22.full$family.agg <- BB22.full$family
+# group all rare families into "other families"
+for(h in rare.families$family){
+  for(i in 1:nrow(BB22.full)){
+    if(BB22.full$family.agg[i] == h){
+      BB22.full$family.agg[i] <- "Other families"
+    }
+  }
+}
+
+# 2. produce data frame with families' abundances per site with leg pollen
+families.site <- BB22.full %>%
+  dplyr::group_by(family.agg, site, bbspecies) %>%
+  dplyr::summarise(cum.abund = sum(Abundance))
+families.site$family.agg <- as.factor(families.site$family.agg)
+
+# 3. plot families per site
+# color palette for families
+palette.fams=c("#07575B", "#5D535E", "#C4DFE6", "#336B87" , "#FAAF08", "#DFE166", "#1995AD", 
+                        "#4897D8", "#80BD9E", "#FA812F", "#66A5AD", "#F34A4A", "#375E97", "#258039",  "#73605B", "#DDBC95",
+                        "#A3A599", "#A1D6E2", "#88A550", "#75B1A9", "#D9B44A", "#4F6457", "#ACD0C0", "#0F1B07", 
+                        "#F7EFE2", "#D09683", "#F62A00", "#A1BE95", "#20948B", "#9B4F0F", "#CB0000")
+                        # filled bar plot per site with abundance of families
+setwd(output)
+ggplot(families.site, aes(fill=family.agg, y=cum.abund, x=site)) + 
+  geom_bar(position="fill", stat="identity")+ 
+  theme_classic(base_size = 20) +
+  facet_wrap(~bbspecies)+ 
+  ggtitle("Plant families per site") +
+  labs(fill='Plant families', x = "sites", y = "relative abundance") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(values=palette.fams, name = "Plant families")
+ggsave(paste("./01_Goal 0/PlantFamilies_per_Site.png", sep = ""), width = 16, height = 8, device = "png")
+
+# 4. produce data frame with families' abundances per region
+families.region <- BB22.full %>%
+  dplyr::group_by(family.agg, region, bbspecies) %>%
+  dplyr::summarise(cum.abund = sum(Abundance))
+families.region$family.agg <- as.factor(families.region$family.agg)
+
+# 5. plot families per region
+ggplot(families.region, aes(fill=family.agg, y=cum.abund, x=region)) + 
+  geom_bar(position="fill", stat="identity")+ 
+  theme_classic(base_size = 20) +
+  facet_wrap(~bbspecies)+ 
+  ggtitle("Plant families per region") +
+  labs(fill='Plant families', x = "regions", y = "relative abundance") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(values=palette.fams, name = "Plant families")
+ggsave(paste("./01_Goal 0/PlantFamilies_per_Region.png", sep = ""), width = 16, height = 8, device = "png", )
+setwd(input) 
+
+#### plot plant growth form per site, region and species
+# 1. produce data frame with exotic/native per site
+ex.nat.site <- BB22.full %>%
+  dplyr::group_by(native_exotic, site, bbspecies) %>%
+  dplyr::summarise(abund = sum(binom.abund))
+ex.nat.site$native_exotic <- as.factor(ex.nat.site$native_exotic)
+
+# 2. plot native/exotic per site
+# create color palette
+palette.ex =c("#518A45", "#BDA509")
+
+setwd(output)
+ggplot(ex.nat.site, aes(fill=native_exotic, y=abund, x=site)) + 
+  geom_bar(position="fill", stat="identity")+ 
+  theme_classic(base_size = 20) +
+  facet_wrap(~bbspecies)+ 
+  ggtitle("Origin status per site LEG") + ylab("Proportion of species in the pollen")+
+  labs(fill='Origin status') +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(values=palette.ex, labels=c('Exotic', 'Native'), name = "")
+ggsave(paste("./01_Goal 0/OriginStatus_per_Site.png", sep = ""), width = 16, height = 8)
+
+# 3. produce data frame with exotic/native per region
+ex.nat.region <- BB22.full %>%
+  dplyr::group_by(native_exotic, region, bbspecies) %>%
+  dplyr::summarise(abund = sum(binom.abund))
+ex.nat.region$native_exotic <- as.factor(ex.nat.region$native_exotic)
+
+# 4. plot native/exotic per region    
+ggplot(ex.nat.region, aes(fill=native_exotic, y=abund, x=region)) + 
+  geom_bar(position="fill", stat="identity")+ 
+  theme_classic(base_size = 20) +
+  facet_wrap(~bbspecies)+ 
+  ggtitle("Origin status per region LEG") + ylab("Proportion of species in the pollen")+
+  labs(fill='Origin status') +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(values=palette.ex, labels=c('Exotic', 'Native'), name="")
+ggsave(paste("./01_Goal 0/OriginStatus_per_Region.png", sep = ""), width = 16, height = 8)
+setwd(input)
+
+
+#### growth type per site and species
+# 1. produce data frame with growth form per site
+growth.site <- BB22.full %>%
+  dplyr::group_by(growth_form_category, site, bbspecies) %>%
+  dplyr::summarise(abund = sum(binom.abund))
+growth.site$growth_form_category <- as.factor(growth.site$growth_form_category)
+
+# 2. plot growth form per site
+# create color palette
+palette.growth =c("#375E97", "#518A45", "#FA812F", "#F34A4A", "#5A99AD", "#A3A3A3")
+
+setwd(output)
+ggplot(growth.site, aes(fill=growth_form_category, y=abund, x=site)) + 
+  geom_bar(position="fill", stat="identity")+ 
+  theme_classic(base_size = 20) +
+  facet_wrap(~bbspecies)+ 
+  ggtitle("Growth Form LEG") + ylab("Proportion of species in the pollen")+ 
+  labs(fill='Growth form') +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(values=palette.growth, name="")
+ggsave(paste("./01_Goal 0/GrowthForm_per_Site.png", sep = ""), width = 16, height = 8)
+
+# 3. produce data frame with growth form per region
+growth.region <- BB22.full %>%
+  dplyr::group_by(growth_form_category, region, bbspecies) %>%
+  dplyr::summarise(abund = sum(binom.abund))
+growth.region$growth_form_category <- as.factor(growth.region$growth_form_category)
+
+# 4. plot growth form per region    
+ggplot(growth.region, aes(fill=growth_form_category, y=abund, x=region)) + 
+  geom_bar(position="fill", stat="identity")+ 
+  theme_classic(base_size = 20) +
+  facet_wrap(~bbspecies)+ 
+  ggtitle("Growth Form LEG") + ylab("Proportion of species in the pollen")+ 
+  labs(fill='Growth form') +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(values=palette.growth, name="")
+ggsave(paste("./01_Goal 0/GrowthForm_per_Region.png", sep = ""), width = 16, height = 8)
+setwd(input)
+
+#### blossom class per site and species
+# 1. produce data frame with blossom class per site
+blossom.site <- BB22.full %>%
+  dplyr::group_by(structural_blossom_class, site, bbspecies) %>%
+  dplyr::summarise(abund = sum(binom.abund))
+blossom.site$structural_blossom_class <- as.factor(blossom.site$structural_blossom_class)
+
+# 2. plot blossom class per site
+# create color palette
+palette.bloss=c("#375E97", "#80BD9E", "#FA812F", "#758A30", "#07575B", "#D95F56", "#C4DFE6")
+
+setwd(output)
+ggplot(blossom.site, aes(fill=structural_blossom_class, y=abund, x=site)) + 
+  geom_bar(position="fill", stat="identity")+ 
+  theme_classic(base_size = 20) +
+  facet_wrap(~bbspecies)+ 
+  ggtitle("Blossom Class per site LEG") + ylab("Proportion of species in the pollen")+
+  labs(fill='Blossom Class') +
+  theme(axis.text.x = element_text(angle = 90))+
+  scale_fill_manual(values=palette.bloss, labels=c('Bell Trumpet', 'Brush', "Dish Bowl", "Flag", "Gullet", "Stalk Disk", "Tube"),
+                    name = "")
+ggsave(paste("./01_Goal 0/BlossomClass_per_Site.png", sep = ""), width = 16, height = 8)
+
+# 3. produce data frame with blossom class per region
+blossom.region <- BB22.full %>%
+  dplyr::group_by(structural_blossom_class, region, bbspecies) %>%
+  dplyr::summarise(abund = sum(binom.abund))
+blossom.region$structural_blossom_class <- as.factor(blossom.region$structural_blossom_class)
+
+# 4. plot blossom class per region
+ggplot(blossom.region, aes(fill=structural_blossom_class, y=abund, x=region)) + 
+  geom_bar(position="fill", stat="identity")+ 
+  theme_classic(base_size = 20) +
+  facet_wrap(~bbspecies)+ 
+  ggtitle("Blossom Class per site LEG") + ylab("Proportion of species in the pollen")+
+  labs(fill='Blossom Class') +
+  theme(axis.text.x = element_text(angle = 90))+
+  scale_fill_manual(values=palette.bloss, labels=c('Bell Trumpet', 'Brush', "Dish Bowl", "Flag", "Gullet", "Stalk Disk", "Tube"),
+                    name = "")
+ggsave(paste("./01_Goal 0/BlossomClass_per_Region.png", sep = ""), width = 16, height = 8)
+setwd(input)
+
+
 #### LEG POLLEN ####
 #### plot plant families per site, region and species
 
@@ -459,8 +653,7 @@ for (i in 1:nrow(BB22.full)) {
   BB22.full$region[i] <- paste(BB22.full$location[i], BB22.full$landscape[i], sep="")
 }
 
-# make a list of all occurring species
-# species <- BB22.full.leg$plant.species
+### LEG POLLEN
 
 # only using leg pollen as it reflects better the bees choice of plant species
 BB22.full.leg <- BB22.full[BB22.full$bborgan=="L",]
@@ -534,6 +727,75 @@ setwd(output)
   ggsave(paste("./01_Goal 0/Phylo_Bubble_Landscape_leg.png", sep = ""), width = 16, height = 16)
   setwd(input)
   
+### BOTH TYPE OF POLLEN
+  # create a data frame with taxa (species, genus, family)
+  phylo <- data.frame(species = BB22.full$plant.species, genus = BB22.full$genus, family = BB22.full$family)
+  
+  # run the phylo-function
+  tree.result <- phylo.maker(phylo, scenarios=c("S1","S2","S3"))
+  
+  # plot the phylogenies with node ages displayed with sceanario 3
+  tree <- plot.phylo(tree.result$scenario.3, cex = 0.5, main = "Phylogenetic tree of species in pollen"); tree
+  
+  # get order of species in tree
+  phylo.order <- data.frame(sps=tree.result$scenario.3$tip.label)
+  phylo.order$order <- seq(1, length(phylo.order$sps))
+  colnames(phylo.order) <- c("plant.species", " order")
+  phylo.order$plant.species <- sub("_", " ", phylo.order$plant.species)
+  
+  # create new data frame with needed variables
+  BB22.full.bubble <- BB22.full%>%
+    dplyr::group_by(site, plant.species, bbspecies)%>%
+    dplyr::summarise(Abundance = sum(Abundance),
+                     region = region,
+                     landscape = landscape)
+  
+  # merge two data frames and order along plant species in phylo-tree
+  BB22.full.bubble_ordered <- merge(x = BB22.full.bubble, y = phylo.order, by.x = "plant.species")%>%
+    mutate(plant.species = as_factor(plant.species))
+  BB22.full.bubble_ordered <- BB22.full.bubble_ordered[order(BB22.full.bubble_ordered$` order`),]
+  BB22.full.bubble_ordered$plant.species <- factor(BB22.full.bubble_ordered$plant.species, 
+                                                       levels = unique(BB22.full.bubble_ordered$plant.species[order(BB22.full.bubble_ordered$` order`)]))
+  
+  # plot bubble plot with relative abundances along order of species in tree
+  setwd(output)
+  # site level
+  library(pals)
+  palette.site <- kelly(18)[3:18] #create color palette for sites
+  ggplot(BB22.full.bubble_ordered, aes(x = site, y =BB22.full.bubble_ordered$plant.species, color = site)) + 
+    geom_point(aes(size = Abundance, fill = site, alpha=0.5)) + 
+    facet_wrap(~bbspecies) +
+    labs(y = "plant species") +
+    theme(axis.text.x = element_text(angle = 90))+
+    theme_classic(base_size = 20) + guides(alpha = "none") +
+    scale_color_manual(values = palette.site, guide = "none")+ #no legend
+    scale_fill_manual(values = palette.site, guide = "none") + #no legend
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  ggsave(paste("./01_Goal 0/Phylo_Bubble_Site.png", sep = ""), width = 16, height = 16)
+  
+  # region level
+  # palette.region <- kelly(18)[10:16] #create color palette for region
+  ggplot(BB22.full.bubble_ordered, aes(x = region, y =BB22.full.bubble_ordered$plant.species)) + 
+    geom_point(aes(size = Abundance, alpha=0.5), color = "#d6851a",) + 
+    facet_wrap(~bbspecies) +
+    labs(y = "plant species")+    
+    theme_classic(base_size = 20) + guides(alpha = "none") #+
+  # scale_color_manual(values = palette.region, guide = "none")+ #no legend
+  # scale_fill_manual(values = palette.region, guide = "none") #no legend
+  ggsave(paste("./01_Goal 0/Phylo_Bubble_Region.png", sep = ""), width = 16, height = 16)
+  
+  # landscape level
+  palette.landscape <- c("#E69F00", "#56B4E9") #create color palette for landscape
+  ggplot(BB22.full.bubble_ordered, aes(x = landscape, y =BB22.full.bubble_ordered$plant.species, color = landscape)) + 
+    geom_point(aes(size = Abundance, fill = landscape, alpha=0.5)) + 
+    facet_wrap(~bbspecies) +
+    labs(y = "plant species")+ 
+    scale_x_discrete(labels=c('rural', 'urban'))+
+    theme_classic(base_size = 20) + guides(alpha = "none") +
+    scale_color_manual(values = palette.landscape, guide = "none") +
+    scale_fill_manual(values = palette.landscape, guide = "none")
+  ggsave(paste("./01_Goal 0/Phylo_Bubble_Landscape.png", sep = ""), width = 16, height = 16)
+  setwd(input)
 
 ############################
 #### LEG/BODY POLLEN DIFFERENCES ####
