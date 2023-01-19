cc8368729d103fa49e86ca456db54ff1b2243039
Bertrand Fournier <bfourni@gmail.com>
2023-01-19 14:17:49 +0100

    Update BB22_compute_FD.R

--- 02_ANALYSIS/BB22_compute_FD.R
+++ 02_ANALYSIS/BB22_compute_FD.R
@@ -104,6 +104,16 @@ trt.pca <- prcomp(traits, scale. = T, center = T)
 cumsum(trt.pca$sdev/sum(trt.pca$sdev))
 trt.scaled <- scores(trt.pca)[,1:2] # adjust number of axes for each group
 
+# NMDS
+trt.NMDS <-
+  metaMDS(scale(traits),
+          distance = "bray",
+          k = 2,
+          maxit = 999, 
+          trymax = 500,
+          wascores = TRUE)
+trt.space.NMDS <- trt.NMDS$spcies   # you can then used this object in the same was as the PCA above   :)
+
 # 2. prepare data frames for mFD-package
 # 2.1 bring plants species per site/ID into wide format (for each BB species)
 library(reshape2)
@@ -232,7 +242,7 @@ big_plot$patchwork
 sp.pa <- as.matrix(sp.pa)
 
 alpha_fd_indices <-mFD::alpha.fd.multidim(
-  sp_faxes_coord   = sp_faxes_coord[ , c("PC1", "PC2", "PC3", "PC4")],
+  sp_faxes_coord   = sp_faxes_coord[ , c("PC1", "PC2", "PC3", "PC4")],  # here use "trt.space.NMDS" instead. All the steps about trait space quality can be skiped.
   asb_sp_w         = sp.pa,
   ind_vect         = c("fdis", "fmpd", "fnnd", "feve", "fric", "fdiv", "fori", 
                        "fspe", "fide"),
