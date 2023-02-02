5cedbfc38bc3e048d938b4b4f1ec3f6411a04266
simonettaselva <sselva@student.ethz.ch>
2023-02-02 15:16:27 +0100

    FD BUMBLEBEES

---
+++ 02_ANALYSIS/BB22_FD_bb.R
@@ -0,0 +1,45 @@
+################################################
+# R compute Functional Diversity Bumblebees
+# by Simonetta Selva
+#
+# Created: December 14, 2022
+# Project: Bumblebee 2022
+################################################
+rm(list=ls())
+
+#load libraries
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(ggpubr)
+
+# set working directory to main repository
+input <- "~/Library/CloudStorage/GoogleDrive-simo1996s@gmail.com/My Drive/ETH/Master Thesis/Bumblebee_2022/01_DATA"
+output <- "~/Library/CloudStorage/GoogleDrive-simo1996s@gmail.com/My Drive/ETH/Master Thesis/Bumblebee_2022/03_OUTPUT"
+
+setwd(input)
+
+# load data
+BB22.bb.traits <- read_csv("BB22_traits.csv")
+
+## data preparation ####
+# replace the names for urban and rural
+for (i in 1:nrow(BB22.bb.traits)) {
+  if (BB22.bb.traits$landscape[i] == "urban") {
+    BB22.bb.traits$landscape[i] = "U"
+  } else {
+    BB22.bb.traits$landscape[i] = "R"
+  }
+}
+
+# rename column site to match other dataframes
+BB22.bb.traits <- BB22.bb.traits %>%
+  mutate(site = paste(location, landscape, replicate, sep = ""),
+         region = paste(location, landscape, sep = "")) %>%
+  dplyr::select(-NrSpecies, -Shannon)
+
+library(reshape2)
+wide <- dcast(BB22.bb.traits, site ~ plant.species, value.var="ab.new")[,-1]
+rownames(wide)  <- levels(BB22_full.ab.new[[j]])
+wide[is.na(wide)] <- 0
+
