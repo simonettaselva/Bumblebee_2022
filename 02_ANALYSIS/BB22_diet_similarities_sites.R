33f5929267904d1b1cd18be16b5e326f3151fb44
simonettaselva <sselva@student.ethz.ch>
2023-03-15 16:29:49 +0100

    summary statistics

--- 02_ANALYSIS/BB22_diet_similarities_sites.R
+++ 02_ANALYSIS/BB22_diet_similarities_sites.R
@@ -101,6 +101,16 @@ setwd(output)
 ggsave("./01_Goal 0/distances/pasc_dist_family.png", width = 10, height = 10)
 setwd(input)
 
+# add mean and SD of distance to a dataframe and export it
+pasc.ID.df.site <- pasc.ID.df %>%
+  group_by(site) %>%
+  summarise(mean.pasc = mean(dist, na.rm = TRUE), 
+            sd.pasc = sd(dist, na.rm = TRUE))
+
+setwd(output)
+write.csv(pasc.ID.df.site, "./01_Goal 0/distances/statistics_pasc.csv")
+setwd(input)
+
 # B.LAPIDARIUS ----
 # only use B.B.lapidarius
 BB22.full.species <- BB22.full[BB22.full$bbspecies == "B.lapidarius",]
@@ -168,3 +178,125 @@ ggplot(lapi.ID.df, aes(x=site, y = dist, fill=landscape)) +
 setwd(output)
 ggsave("./01_Goal 0/distances/lapi_dist_family.png", width = 10, height = 10)
 setwd(input)
+
+# add mean and SD of distance to a dataframe and export it
+lapi.ID.df.site <- lapi.ID.df %>%
+  group_by(site) %>%
+  summarise(mean.lapi = mean(dist, na.rm = TRUE), 
+            sd.lapi = sd(dist, na.rm = TRUE))
+
+setwd(output)
+write.csv(lapi.ID.df.site, "./01_Goal 0/distances/statistics_lapi.csv")
+setwd(input)
+
+
+# compare species summary statistics ----
+library(stats)
+library(tidyverse)
+library(rstatix)
+library(ggpubr)
+
+site.summary <- data.frame(site = rep(lapi.ID.df.site$site, 2))
+site.summary <- site.summary %>%
+  mutate(bbspecies = c(rep("B.lapidarius", 16), rep("B.pascuorum", 16)),
+         landscape = substring(site, 3,3),
+         mean = c(lapi.ID.df.site$mean.lapi, pasc.ID.df.site$mean.pasc))
+
+# subset for different comparisons
+site.summary.lapi <- site.summary %>% filter(bbspecies == "B.lapidarius")
+site.summary.pasc <- site.summary %>% filter(bbspecies == "B.pascuorum")
+site.summary.urban <- site.summary %>% filter(landscape == "U")
+site.summary.rural <- site.summary %>% filter(landscape == "R")
+
+# prepare list for plots
+plot.list <- list()
+
+## compare the mean of landscape for B.lapidarius ----
+res.aov1 <- site.summary.lapi %>% anova_test(mean ~ landscape)
+res.aov1
+pwc1 <- site.summary.lapi %>%
+  pairwise_t_test(mean ~ landscape, p.adjust.method = "bonferroni")
+pwc1
+
+# Show adjusted p-values
+pwc1 <- pwc1 %>% add_xy_position(x = "landscape")
+plot.list[[1]] <-
+  ggboxplot(site.summary.lapi, x = "landscape", y = "mean", fill = "landscape", notch = TRUE) +
+  stat_pvalue_manual(pwc1, label = "p.adj", tip.length = 0, step.increase = 0.1) +
+  labs(subtitle = get_test_label(res.aov1, detailed = TRUE)) +
+  ggtitle("B.lapidarius") + # for the main title
+  xlab("landscape")+
+  scale_x_discrete(labels=c('rural', 'urban'))+
+  theme_classic(base_size = 20) +     
+  theme(aspect.ratio=1)+
+  scale_fill_manual(values=c("#E69F00", "#56B4E9"))
+
+## compare the mean of landscape for B.pascuorum ----
+res.aov2 <- site.summary.pasc %>% anova_test(mean ~ landscape)
+res.aov2
+pwc2 <- site.summary.pasc %>%
+  pairwise_t_test(mean ~ landscape, p.adjust.method = "bonferroni")
+pwc2
+
+# Show adjusted p-values
+pwc2 <- pwc2 %>% add_xy_position(x = "landscape")
+plot.list[[2]] <-
+  ggboxplot(site.summary.pasc, x = "landscape", y = "mean", fill = "landscape", notch = TRUE) +
+  stat_pvalue_manual(pwc2, label = "p.adj", tip.length = 0, step.increase = 0.1) +
+  labs(subtitle = get_test_label(res.aov2, detailed = TRUE)) +
+  ggtitle("B.pascuorum") + # for the main title
+  xlab("landscape")+
+  scale_x_discrete(labels=c('rural', 'urban'))+
+  theme_classic(base_size = 20) +     
+  theme(aspect.ratio=1)+
+  scale_fill_manual(values=c("#E69F00", "#56B4E9")) 
+
+
+## compare the mean of species in urban areas ----
+res.aov3 <- site.summary.urban %>% anova_test(mean ~ bbspecies)
+res.aov3
+pwc3 <- site.summary.urban %>%
+  pairwise_t_test(mean ~ bbspecies, p.adjust.method = "bonferroni")
+pwc3
+
+# Show adjusted p-values
+pwc3 <- pwc3 %>% add_xy_position(x = "bbspecies")
+plot.list[[3]] <-
+  ggboxplot(site.summary.urban, x = "bbspecies", y = "mean", fill = "bbspecies", notch = TRUE) +
+  stat_pvalue_manual(pwc3, label = "p.adj", tip.length = 0, step.increase = 0.1) +
+  labs(subtitle = get_test_label(res.aov3, detailed = TRUE)) +
+  ggtitle("Urban areas") + # for the main title
+  xlab("Bumblebee species")+
+  scale_x_discrete(labels=c('B.lapidarius', 'B.pascuorum'))+
+  theme_classic(base_size = 20) +     
+  theme(aspect.ratio=1)+
+  scale_fill_manual(values=c("#291600","#e0b802")) 
+
+## compare the mean of species in rural areas ----
+res.aov4 <- site.summary.rural %>% anova_test(mean ~ bbspecies)
+res.aov4
+pwc4 <- site.summary.rural %>%
+  pairwise_t_test(mean ~ bbspecies, p.adjust.method = "bonferroni")
+pwc4
+
+# Show adjusted p-values
+pwc4 <- pwc4 %>% add_xy_position(x = "bbspecies")
+plot.list[[4]] <-
+  ggboxplot(site.summary.rural, x = "bbspecies", y = "mean", fill = "bbspecies", notch = TRUE) +
+  stat_pvalue_manual(pwc4, label = "p.adj", tip.length = 0, step.increase = 0.1) +
+  labs(subtitle = get_test_label(res.aov4, detailed = TRUE)) +
+  ggtitle("Rural areas") + # for the main title
+  xlab("Bumblebee species")+
+  scale_x_discrete(labels=c('B.lapidarius', 'B.pascuorum'))+
+  theme_classic(base_size = 20) +     
+  theme(aspect.ratio=1)+
+  scale_fill_manual(values=c("#291600","#e0b802")) 
+
+# arrange them into one file to export
+plot <- ggarrange(plot.list[[1]], plot.list[[2]],
+                  plot.list[[3]], plot.list[[4]],
+                  ncol = 2, nrow = 2,
+                  labels = c("A", "B", "C", "D"))
+ggsave("./01_Goal 0/distances/summary_stats_overview.png", width = 20, height = 20)
+setwd(input)
+
